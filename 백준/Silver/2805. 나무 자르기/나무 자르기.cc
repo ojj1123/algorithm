e43b076159da88d74939cb8bd2c471be792d4b5b
Jeongjin Oh <rojay.developer@gmail.com>
2023-07-25 21:46:21 +0900

    [Silver II] Title: 나무 자르기, Time: 156 ms, Memory: 41084 KB -BaekjoonHub

---
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.cc
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+
+#define endl '\n'
+using namespace std;
+using ll = long long;
+
+const int INF = 10e9;
+const int MAX = 10e6;
+
+int arr[MAX];
+int n, m, maxTree;
+
+void init();
+int binarySearch(int, int, int);
+
+int main() {
+    ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+    init();
+    cout << binarySearch(1, maxTree, m);
+}
+
+void init() {
+    cin >> n >> m;
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+        maxTree = max(arr[i], maxTree);
+    }
+}
+
+bool check(int currentLength) {
+    ll sum = 0;
+    for (int i = 0; i < n; i++) {
+        if (currentLength >= arr[i]) continue;
+        sum += arr[i] - currentLength;
+    }
+    return sum >= m;
+}
+int binarySearch(int start, int end, int target) {
+    if (start > end) return end;
+
+    int mid = (start + end) / 2;
+    if (check(mid)) return binarySearch(mid + 1, end, target);
+    return binarySearch(start, mid - 1, target);
+}
