9997b04ab5c8a2b07344b61fdecbe9a1a3fda707
Jeongjin Oh <rojay.developer@gmail.com>
2023-07-27 23:20:41 +0900

    [Bronze I] Title: 소인수분해, Time: 32 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/11653. 소인수분해/소인수분해.cc
+++ 백준/Bronze/11653. 소인수분해/소인수분해.cc
@@ -3,15 +3,10 @@
 using namespace std;
 #define endl '\n'
 
-const int MAX = 1e7;
-
 int n;
-vector<pair<int, int>> vec;
-bool isPrime[MAX + 2];
 
 void init();
 void solve();
-int gcd(int, int);
 
 int main() {
     ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
@@ -21,38 +16,16 @@ int main() {
 
 void init() {
     cin >> n;
-    fill(isPrime, isPrime + n + 1, true);
 }
 
 void solve() {
     if (n == 1) return;
 
-    isPrime[1] = false;
-    isPrime[2] = true;
-
-    for (int i = 2; i * i <= n; i++) {
-        if (isPrime[i]) continue;
-        for (int j = i * i; j <= n; j += i) {
-            isPrime[j] = false;
-        }
-    }
-
     int temp = n;
     for (int i = 2; i <= n; i++) {
-        if (!isPrime[i]) continue;
-        if (temp < i) break;
-        int cnt = 0;
         while (temp % i == 0) {
+            cout << i << endl;
             temp /= i;
-            cnt++;
-        }
-        if (cnt == 0) continue;
-        vec.push_back({i, cnt});
-    }
-    int len = vec.size();
-    for (int i = 0; i < len; i++) {
-        for (int j = 0; j < vec[i].second; j++) {
-            cout << vec[i].first << endl;
         }
     }
 }
