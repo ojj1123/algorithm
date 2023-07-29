8981ad9f674c793a237b184baa80fe72e45f7065
Jeongjin Oh <rojay.developer@gmail.com>
2023-07-29 23:59:22 +0900

    [Silver II] Title: N과 M (9), Time: 12 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/15663. N과 M （9）/N과 M （9）.cc
+++ 백준/Silver/15663. N과 M （9）/N과 M （9）.cc
@@ -1,19 +1,12 @@
 #include <bits/stdc++.h>
 
 using namespace std;
-using ll = long long;
 #define endl '\n'
-
-const int INF = 1e9;
-const int MAX = 1e7;
 
 int n, m;
 int input[10];
-string strInput[10];
 bool isUsed[10];
-vector<string> arr;
-set<string> check;
-vector<string> output;
+int check[10];
 
 void init();
 void solve(int);
@@ -22,34 +15,29 @@ int main() {
     ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
     init();
     solve(0);
-    for (auto it = output.begin(); it != output.end(); it++) cout << *it << endl;
 }
 
 void init() {
     cin >> n >> m;
     for (int i = 0; i < n; i++) cin >> input[i];
     sort(input, input + n);
-    for (int i = 0; i < n; i++) strInput[i] = to_string(input[i]);
 }
 
 void solve(int cur) {
     if (cur >= m) {
-        string str = "";
         for (int i = 0; i < m; i++) {
-            str += arr[i] + ((i != m - 1) ? " " : "");
+            cout << input[check[i]] << ' ';
         }
-        if (check.count(str) == 0) {
-            check.insert(str);
-            output.push_back(str);
-        }
+        cout << endl;
         return;
     }
+    int temp = -1;
     for (int i = 0; i < n; i++) {
-        if (isUsed[i]) continue;
+        if (isUsed[i] || input[i] == temp) continue;
+        check[cur] = i;
+        temp = input[check[cur]];
         isUsed[i] = true;
-        arr.push_back(strInput[i]);
         solve(cur + 1);
         isUsed[i] = false;
-        arr.pop_back();
     }
 }
