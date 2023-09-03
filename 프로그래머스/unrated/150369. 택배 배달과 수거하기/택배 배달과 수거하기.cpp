08d899e1d390d649ad7db5b313c23ccc83234922
Jeongjin Oh <rojay.developer@gmail.com>
2023-09-03 18:46:32 +0900

    Update 택배 배달과 수거하기.cpp

--- 프로그래머스/unrated/150369. 택배 배달과 수거하기/택배 배달과 수거하기.cpp
+++ 프로그래머스/unrated/150369. 택배 배달과 수거하기/택배 배달과 수거하기.cpp
@@ -25,35 +25,18 @@ long long solution(int cap, int n, vector<int> deliveries, vector<int> pickups) 
         }
     }
     
-    int dSum = 0, pSum = 0;
     int dCap, pCap;
     while(true) {
         if(dIdx == -1 && pIdx == -1) break;
         
         answer += 2 * max(dIdx + 1, pIdx + 1);
-        // cout << dIdx << ' ' << pIdx << endl;
-        // cout << dIdx << ':' << deliveries[dIdx] << endl;
-        // cout<<answer<<endl;
         
         
         dCap = cap;
         pCap = cap;
-        // dSum = 0;
-        // pSum = 0;
         
         if(dIdx != -1) {
             while(dIdx > -1) {
-                // if(deliveries[dIdx] > cap) {
-                //     deliveries[dIdx] -= cap;
-                //     break;
-                // }
-                // dSum += deliveries[dIdx];
-                // if(dSum > cap) {
-                //     dSum -= deliveries[dIdx];
-                //     break;
-                // }
-                // deliveries[dIdx] = 0;
-                // dIdx--;
                 if(deliveries[dIdx] > dCap) {
                     deliveries[dIdx] -= dCap;
                     break;
@@ -70,17 +53,6 @@ long long solution(int cap, int n, vector<int> deliveries, vector<int> pickups) 
         
         if(pIdx != -1) {
             while(pIdx > -1) {
-                // if(pickups[pIdx] > cap) {
-                //     pickups[pIdx] -= cap;
-                //     break;
-                // }
-                // pSum += pickups[pIdx];
-                // if(pSum > cap) {
-                //     pSum -= pickups[pIdx];
-                //     break;
-                // }
-                // pickups[pIdx] = 0;
-                // pIdx--;
                 if(pickups[pIdx] > pCap) {
                     pickups[pIdx] -= pCap;
                     break;
