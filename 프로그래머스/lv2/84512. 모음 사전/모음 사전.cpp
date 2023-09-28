f762a110bfaae0ca27defec522ebd15c282c8098
Jeongjin Oh <rojay.developer@gmail.com>
2023-09-28 19:29:50 +0900

    [level 2] Title: 모음 사전, Time: 1.35 ms, Memory: 3.76 MB -BaekjoonHub

--- 프로그래머스/lv2/84512. 모음 사전/모음 사전.cpp
+++ 프로그래머스/lv2/84512. 모음 사전/모음 사전.cpp
@@ -1,25 +1,24 @@
 #include <string>
 #include <vector>
 #include <algorithm>
-#include <iostream>
+
 using namespace std;
 
 vector<string> alpha = {"A", "E", "I", "O", "U"};
 vector<string> result;
 
-void brute(int,int,string);
+void brute(int, int, string);
 
 int solution(string word) {
     int answer = 0;
-    for(int i=1;i<=5;i++) {
+    for(int i = 1;i <= 5;i++) {
         brute(i, 0, "");
     }
     sort(result.begin(), result.end());
     
-    for(int i=0;i<result.size();i++) {
+    for(int i = 0;i < result.size();i++) {
         if(word.compare(result[i]) == 0) return i + 1;
     }
-    // return answer;
 }
 
 // cnt - 문자 개수, k - 지금까지 추가한 문자 개수
@@ -29,9 +28,9 @@ void brute(int cnt, int k, string str) {
         return;
     }
     
-    for(int i=0;i<5;i++) {
+    for(int i = 0;i < 5;i++) {
         str += alpha[i];
-        brute(cnt, k+1, str);
+        brute(cnt, k + 1, str);
         str.pop_back();
     }
 }
