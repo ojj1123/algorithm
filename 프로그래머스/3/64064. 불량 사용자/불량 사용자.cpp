6f76fdafeac40c5c72facd292b1d61be5fd79992
Jeongjin Oh <rojay.developer@gmail.com>
2023-10-13 22:30:57 +0900

    [level 3] Title: 불량 사용자, Time: 0.03 ms, Memory: 4.14 MB -BaekjoonHub

--- 프로그래머스/3/64064. 불량 사용자/불량 사용자.cpp
+++ 프로그래머스/3/64064. 불량 사용자/불량 사용자.cpp
@@ -5,56 +5,45 @@
 using namespace std;
 
 bool isCheck[10];
-vector<int> graph[10];
 unordered_set<string> dedup;
 
-void brute(int, int);
+void brute(vector<string>, vector<string>, int);
+bool check(string, string);
 
 int solution(vector<string> user_id, vector<string> banned_id) {
     int answer = 0;
-    int userLen = user_id.size();
-    int bannedLen = banned_id.size();
     
-    for(int k = 0;k < bannedLen;k++) {
-        int bannedUserNameLen = banned_id[k].length();
-        for(int num = 0;num < userLen;num++) {
-            int userNameLen = user_id[num].length();
-            if(bannedUserNameLen != userNameLen) continue;
-            
-            bool flag = true;
-            for(int i = 0;i < userNameLen;i++) {
-                if(banned_id[k][i] == '*') continue;
-                if(banned_id[k][i] != user_id[num][i]) {
-                    flag = false;
-                    break;
-                }
-            }
-            if(flag) {
-                graph[k].push_back(num);
-            }
-        }
-    }
-    brute(bannedLen, 0);
+    brute(user_id, banned_id, 0);
     answer = dedup.size();
     return answer;
 }
 
 string str = "";
-void brute(int total, int curIdx) {
-    if(curIdx >= total) {
+void brute(vector<string> user_id, vector<string> banned_id, int curIdx) {
+    if(curIdx >= banned_id.size()) {
         string temp = str;
         sort(temp.begin(), temp.end());
         dedup.insert(temp);
         return;
     }
     
-    for(int i = 0;i < graph[curIdx].size();i++) {
-        int user = graph[curIdx][i];
-        if(isCheck[user]) continue;
-        isCheck[user] = true;
-        str += to_string(user);
-        brute(total, curIdx + 1);
+    for(int i = 0;i < user_id.size();i++) {
+        if(isCheck[i] || !check(user_id[i], banned_id[curIdx])) continue;
+        isCheck[i] = true;
+        str += to_string(i);
+        brute(user_id, banned_id, curIdx + 1);
         str.pop_back();
-        isCheck[user] = false;
+        isCheck[i] = false;
     }
+}
+
+bool check(string user, string bannedUser) {
+    int userLen = user.length();
+    int bannedUserLen = bannedUser.length();
+    if(userLen != bannedUserLen) return false;
+    for(int i = 0;i < userLen;i++) {
+        if(bannedUser[i] == '*') continue;
+        if(bannedUser[i] != user[i]) return false;
+    }
+    return true;
 }
