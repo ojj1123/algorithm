dac12921537e4b72d617d5dfa532e86bd776e01e
Jeongjin Oh <rojay.developer@gmail.com>
2024-04-09 12:54:00 +0800

    [level 3] Title: 주사위 고르기, Time: 1241.71 ms, Memory: 44.7 MB -BaekjoonHub

---
+++ 프로그래머스/3/258709. 주사위 고르기/주사위 고르기.js
@@ -0,0 +1,88 @@
+
+
+function solution(dice) {
+    let answer = [];
+    let n = dice.length;
+    let isUsed = Array.from(Array(n), () => false)
+    let arrA = []
+    let arrB = []
+    let order = []
+    let maxVal = 0
+    
+    brute(0, 0);
+    
+    // 어떤 주사위 사용할지
+    function brute(cur, cnt) {
+        if(cnt >= n / 2) {
+            arrA = []
+            arrB = []
+            calculateDiceSum(0)
+            
+            arrA.sort(compare)
+            arrB.sort(compare)
+            
+            let startB = 0
+            
+            let count = 0
+            for(let i = 0;i < arrA.length;i++) {
+                while(startB < arrB.length) {
+                    if(arrA[i] > arrB[startB]) {
+                        break
+                    }
+                    startB++
+                }
+                count += arrB.length - startB + 1
+            }
+            
+            if(maxVal < count) {
+                maxVal = count
+                answer = []
+                for(let i = 0;i < n;i++) {
+                    if(!isUsed[i]) continue;
+                    answer.push(i + 1)
+                }
+            }
+            
+            return;
+        }
+        for(let i = cur; i < n; i++) {
+            isUsed[i] = true
+            brute(i + 1, cnt + 1)
+            isUsed[i] = false
+        }
+    }
+    
+    // 선택한 주사위에서 나올 수 있는 경우의 수(굴린 주사위 합) 모두 구하기
+    function calculateDiceSum(cnt) {
+        if(cnt >= n / 2) {
+            let startA = 0
+            let startB = 0
+            let sumA = 0
+            let sumB = 0
+            for(let i = 0;i < n;i++) {
+                if(isUsed[i]) { // A
+                    sumA += dice[i][order[startA++]]
+                } else { // B
+                    sumB += dice[i][order[startB++]]
+                }
+            }
+            arrA.push(sumA)
+            arrB.push(sumB)
+            return
+        }
+        
+        for(let i = 0;i < 6;i++) {
+            order.push(i)
+            calculateDiceSum(cnt + 1)
+            order.pop();
+        }
+    }
+    
+    return answer;
+}
+
+function compare(a, b) {
+    if(a < b) return 1
+    if(a === b) return 0
+    if(a > b) return -1
+}
