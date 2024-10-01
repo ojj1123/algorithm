e917c603c87e0434dd76bfbc2c48e0b31aeb327e
Jeongjin Oh <rojay.developer@gmail.com>
2024-10-01 16:12:24 +0900

    [level 3] Title: [PCCP 기출문제] 4번 / 수식 복원하기, Time: 12.24 ms, Memory: 83 MB -BaekjoonHub

--- 프로그래머스/3/340210. ［PCCP 기출문제］ 4번 ／ 수식 복원하기/［PCCP 기출문제］ 4번 ／ 수식 복원하기.java
+++ 프로그래머스/3/340210. ［PCCP 기출문제］ 4번 ／ 수식 복원하기/［PCCP 기출문제］ 4번 ／ 수식 복원하기.java
@@ -2,78 +2,67 @@ import java.util.*;
 
 class Solution {
     public String[] solution(String[] expressions) {
-        ArrayList<String> answer = new ArrayList<>();
-        boolean[] check = new boolean[10];
-        Arrays.fill(check, true);
+        ArrayList<String> unknownExp = new ArrayList<>();
+        ArrayList<Integer> radixList = new ArrayList<>();
         
         for(String expression: expressions) {
-            String[] arr = expression.split(" \\+ | - | = ");
-            boolean[] checkEach = new boolean[10];
-            Arrays.fill(checkEach, false);
-            
-            for(int radix = 2;radix <= 9;radix++) {
-                // 해당 수식이 해당 진법이 가능한가?
-                boolean flag = false;
-                for(int j = 0;j < arr.length;j++) {
-                    if(arr[j].equals("X")) continue;
-                    if(!canUseRadix(arr[j], radix)) {
-                        flag = true;
-                        break;
-                    }
-                }
-                if(flag) continue;
-                // 정답이 없는 경우) 해당 진법 가능함
-                if(arr[2].equals("X")) {
-                    checkEach[radix] = true;
-                    continue;
-                }
-                // 정답이 있는 경우) 가능한 진법으로 정답도 동일한가?
-                String res = "";
-                if(expression.contains("+")) {
-                    res = Integer.toString(Integer.parseInt(arr[0], radix) + Integer.parseInt(arr[1], radix), radix);
-                } else {
-                    res = Integer.toString(Integer.parseInt(arr[0], radix) - Integer.parseInt(arr[1], radix), radix);
-                }
-                checkEach[radix] = res.equals(arr[2]);
-            }
-            for(int radix = 2;radix <= 9;radix++) {
-                check[radix] = check[radix] && checkEach[radix];
-            }
+            if(expression.contains("X")) unknownExp.add(expression);
         }
         
-        for(String exp: expressions) {
-            if(!exp.contains("X")) continue;
+        for(int radix = 2;radix < 10;radix++) {
+            if(validate(radix, expressions)) radixList.add(radix);
+        }
+        
+        String[] answer = new String[unknownExp.size()];
+        
+        for(int i = 0;i < answer.length;i++) {
+            String expression = unknownExp.get(i);
+            HashSet<String> resultSet = new HashSet<>();
+            String[] split = expression.split("\\s+?=\\s+?");
+            String result = "";
             
-            String[] arr = exp.split(" \\+ | - | = ");
-            HashSet<String> valSet = new HashSet<>();
-            
-            for(int radix = 2;radix <= 9;radix++) {
-                if(!check[radix]) continue;
-                
-                String res = "";
-                if(exp.contains("+")) {
-                    res = Integer.toString(Integer.parseInt(arr[0], radix) + Integer.parseInt(arr[1], radix), radix);
-                } else {
-                    res = Integer.toString(Integer.parseInt(arr[0], radix) - Integer.parseInt(arr[1], radix), radix);
-                }
-                valSet.add(res);
+            for(Integer radix: radixList) {
+                int value = calc(split[0], radix);
+                result = Integer.toString(value, radix);
+                resultSet.add(result);
             }
             
-            ArrayList<String> list = new ArrayList<>(valSet);
-            if(list.size() == 1) {
-                answer.add(exp.replace("X", list.get(0)));
+            if(resultSet.size() == 1) {
+                answer[i] = expression.replace("X", result);
             } else {
-                answer.add(exp.replace("X", "?"));
+                answer[i] = expression.replace("X", "?");
             }
         }
         
-        return answer.toArray(new String[answer.size()]);
+        return answer;
     }
     
-    public boolean canUseRadix(String numStr, int radix) {
-        for(int i = 0;i < numStr.length();i++) {
-            if(numStr.charAt(i) - '0' >= radix) return false;
+    public boolean validate(int radix, String[] expressions) {
+        try {
+            for(String expression: expressions) {
+                String[] split = expression.split("\\s+?=\\s+?");
+                int result = calc(split[0], radix);
+                // X가 없다면 결과값이랑 비교
+                if(!split[1].equals("X")) {
+                    int res = Integer.parseInt(split[1], radix);
+                    if(result != res) return false;
+                }
+            }
+        } catch(NumberFormatException e) {
+            return false;
         }
         return true;
+    }
+    
+    public int calc(String expression, int radix) {
+        String[] split = expression.split(" ");
+        int num1 = Integer.parseInt(split[0], radix);
+        int num2 = Integer.parseInt(split[2], radix);
+        String op = split[1];
+        
+        if(op.equals("+")) {
+            return num1 + num2;
+        }
+        return num1 - num2;
     }
 }
