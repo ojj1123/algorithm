eb586c510f431276beb46dd1ab9668bfe1ce7749
Jeongjin Oh <rojay.developer@gmail.com>
2024-10-23 17:23:39 +0900

    [level 3] Title: 미로 탈출 명령어, Time: 6.75 ms, Memory: 36.6 MB -BaekjoonHub

--- 프로그래머스/3/150365. 미로 탈출 명령어/미로 탈출 명령어.js
+++ 프로그래머스/3/150365. 미로 탈출 명령어/미로 탈출 명령어.js
@@ -5,36 +5,33 @@ const dirStr = ['d', 'l', 'r', 'u'];
 function solution(n, m, x, y, r, c, k) {
     let answer = '';
     
-    brute(0, k, x, y, '');
+    if(!possible(x, y, r, c, k)) return 'impossible';
     
-    function brute(curK, remainK, curX, curY, path) {
-        if(answer != '') return;
-        if(curK >= k) { // k번 이동 완료
-            if(curX == r && curY == c) {
-                answer = path;
-            }
-            return;
-        }
-        const dist = getDist(curX, curY, r, c);
-        if(dist > remainK) {
-            return;
-        }
-        if((dist % 2 == 0 && remainK % 2 == 1) || (dist % 2 == 1 && remainK % 2 == 0)) {
-            return;
-        }
-
+    let curX = x;
+    let curY = y;
+    let curK = k;
+    while(curK > 0) {
         for(let dir = 0;dir < 4;dir++) {
-            if(answer != '') return;
             const nx = curX + dx[dir];
             const ny = curY + dy[dir];
             if(nx <= 0 || nx > n || ny <= 0 || ny > m) continue;
-            brute(curK + 1, remainK - 1, nx, ny, path + dirStr[dir]);
+            if(possible(nx, ny, r, c, curK - 1)) {
+                answer += dirStr[dir];
+                curX = nx;
+                curY = ny;
+                curK--;
+                break;                
+            }
         }
     }
     
-    return answer == '' ? 'impossible' : answer;
+    return answer;
 }
 
-function getDist(x1, y1, x2, y2) {
-    return Math.abs(x1 - x2) + Math.abs(y1 - y2);
+function possible(x, y, r, c, k) {
+    const distance = Math.abs(x - r) + Math.abs(y - c);
+    if(distance > k) return false;
+    if(distance == k) return true;
+    if(distance % 2 != k % 2) return false;
+    return true;
 }
